b859ac072f33bb8dc8beb8f211543bfc62c670fd
Catherine Duval <catherine@bamboosoft.ca>
2023-05-19 09:58:48 -0400

    working bamboo stick manager with basic styling

---
+++ src/app/components/tasks/tasks.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit } from '@angular/core';
+import {TaskService} from '../../services/task.service'
+import {Task} from '../../Task';
+
+@Component({
+  selector: 'app-tasks',
+  templateUrl: './tasks.component.html',
+  styleUrls: ['./tasks.component.css']
+})
+export class TasksComponent {
+
+  tasks: Task[] = [];
+
+  constructor(private taskService: TaskService) {
+  }
+
+  ngOnInit(): void {
+    this.taskService.getTasks().subscribe((tasks) => this.tasks = tasks);
+
+  }
+
+  deleteTask(task: Task){
+    this.taskService
+    .deleteTask(task)
+    .subscribe(
+      () => {
+        console.log('test');
+        this.tasks = this.tasks.filter((t) => t.id !== task.id);
+      } 
+    )
+
+  }
+
+  toggleReminder(task: Task){
+    task.reminder = !task.reminder;
+    this.taskService.updateTaskReminder(task).subscribe();
+    console.log(task.reminder)
+  }
+
+  addTask(task: Task) {
+    this.taskService.addTask(task).subscribe((task) => (this.tasks.push(task)));
+  }
+
+
+
+}
